5f8991752d4f5bc26eaa588202cee2db26bf978e
leviethoanganh <165462700+leviethoanganh@users.noreply.github.com>
2025-04-17 14:23:44 +0800

    Update ManagerUI.java

--- ManagerUI.java
+++ ManagerUI.java
@@ -15,7 +15,7 @@ public class ManagerUI implements ManagerMenu {
 
     public void logout() {
         EnquiryHandler.syncEnquiriesOnLogout(Database.enquiryList, manager.getEnquiryHandler().getEnquiryList());
-        CSVWriter.saveEnquirieToCSV(Database.enquiryList, "EnquiryList.csv");
+  //    CSVWriter.saveEnquirieToCSV(Database.enquiryList, "EnquiryList.csv");
         System.out.println("You have successfully logged out.");
     }
 
